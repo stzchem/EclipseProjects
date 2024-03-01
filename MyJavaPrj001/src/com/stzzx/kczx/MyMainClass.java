1491bd263c7cf196e20f8411811e44cb2903b372
stzchem <stzchem@163.com>
2024-03-01 20:03:38 +0800

    This is the second commit!

--- MyJavaPrj001/src/com/stzzx/kczx/MyMainClass.java
+++ MyJavaPrj001/src/com/stzzx/kczx/MyMainClass.java
@@ -4,7 +4,8 @@ public class MyMainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Just a test!");
+		for(int i=0;i<10;i++)
+			System.out.println("Just a test!");
 	}
 
 }
