963494f31c98a8d6262b965653391e3436a7629c
stzchem <stzchem@163.com>
2024-03-01 19:59:39 +0800

    My First Java Projects!

---
+++ MyJavaPrj001/src/com/stzzx/kczx/MyMainClass.java
@@ -0,0 +1,10 @@
+package com.stzzx.kczx;
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just a test!");
+	}
+
+}
